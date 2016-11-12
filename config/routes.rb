64b36549cce66b2b97ca361563c452a041ce89cc
Viet Anh Nguyen <vank58.bk@gmail.com>
2016-11-12 09:23:33 +0700

    complete homework week 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
